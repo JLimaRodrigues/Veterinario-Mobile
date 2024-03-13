45aa98763458d1d13eefed208a15025beef9b61f
Jefferson Lima <110677208+JLimaRodrigues@users.noreply.github.com>
2024-03-13 16:47:39 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import { PaperProvider } from 'react-native-paper';
 import 'react-native-gesture-handler';
 
 import Main from './components/Main'
 
 export default function App() {
   return (
+    <PaperProvider>
       <Main />
+    </PaperProvider>
   );
 }
